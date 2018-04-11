566274e76e5699483e6de4b3845979e88c326420
刘鹤 <liuhe@csdn.net>
2018-04-11 10:44:31 +0800

    持续集成环境测试

--- test/four/curry.test.js
+++ test/four/curry.test.js
@@ -6,19 +6,59 @@ let increment = locacurry.increment;
 let addTen = locacurry.addTen;
 let match = locacurry.match;
 let replace = locacurry.replace;
+let filter = locacurry.filter;
+let map = locacurry.map;
+let hasSpaces = locacurry.hasSpaces;
+let findSpaces = locacurry.findSpaces;
+let noVowels = locacurry.noVowels;
+let censored = locacurry.censored;
 
 describe('第四章 柯里化', function(){
-  it('increment add 2 equal 3', function(){
-    should(increment(2)).be.equal(3);
+  describe('unit four start example', function(){
+    it('increment add 2 equal 3', function(){
+      should(increment(2)).be.equal(3);
+    });
+    it('addTen add 10 equal 20', function(){
+      should(addTen(10)).be.equal(20);
+    });
   });
-  it('addTen add 10 equal 20', function(){
-    should(addTen(10)).be.equal(20);
+  describe('match', function(){
+    it('match return array', function() {
+      should(match(/\s+/g, "hello world")).be.array;
+    });
+    
+    it('hasSpaces return array', function() {
+      should(hasSpaces( "hello world")).be.array;
+    });
   });
-  it('match return array', function() {
-    should(match(/\s+/g, "hello world")).be.array;
-  })
-  var hasSpaces = match(/\s+/g);
-  it('hasSpaces return array', function() {
-    should(hasSpaces( "hello world")).be.array;
-  })
+  describe('replace', function(){
+    it('replace optput ok is a string',function(){
+      should(replace(/\s+/g,' ','ok')).be.equal('ok').be.string;
+    });
+    
+    let replaceSpaces = replace(/\s+/g);
+    it('replaceSpaces optput ok is a string',function(){
+      should(replaceSpaces(' ','ok')).be.equal('ok').be.string;
+    });
+    
+    it('noVowels output string',function(){
+      should(noVowels("Chocolate Rain","*")).be.equal('*').be.string;
+    })
+    it('censored output string',function(){
+      should(censored("Chocolate Rain")).be.equal('Ch*c*l*t* R**n').be.string;
+    })
+  });
+  describe('filter',function(){
+    it('filter output array',function(){
+      should(filter(hasSpaces, ["tori_spelling", "tori amos"])).be.array;
+    });
+    it('findSpaces output array', function(){
+      should(findSpaces(["tori_spelling", "tori amos"])[0]).be.equal('tori amos').be.string;
+    });
+  });
+  describe('map',function(){
+    it('map output array',function(){
+      should(map(function(x){return x=== 1 ? true : false},[1,1,2])).be.array;
+    });
+  });
 });
--- unit/four/curry.js
+++ unit/four/curry.js
@@ -22,19 +22,71 @@ let match = curry(function(what,str) {
 
 /**
  * 查找替换
- * @param  {[boolean]} what        [description]
+ * @param  {[RegExp]} what        [description]
  * @param  {[string]} replacement [description]
  * @param  {[string]} str         [description]
  * @return {[string]}             [description]
  */
 let replace = curry(function(what, replacement, str) {
-  return str.replace(what, replacement)
+  return str.replace(what, replacement);
 });
 
+/**
+ * [description]
+ * @param  {[function]} f   [description]
+ * @param  {[array]} ary [description]
+ * @return {[array]}     [description]
+ */
+let filter = curry(function(f,ary) {
+  return ary.filter(f);
+})
 
+/**
+ * [description]
+ * @param  {[function]} f   [description]
+ * @param  {[array]} ary [description]
+ * @return {[array]}     [description]
+ */
+let map = curry(function(f,ary) {
+  return ary.map(f);
+})
+
+/**
+ * [hasSpaces description]
+ * @type {Boolean}
+ */
+let hasSpaces = match(/\s+/g);
+
+hasSpaces("hello world");
+// [ ' ' ]
+hasSpaces("spaceless");
+// null
+
+filter(hasSpaces, ["tori_spelling", "tori amos"]);
+// ["tori amos"]
+
+let findSpaces = filter(hasSpaces);
+// function(xs) { return xs.filter(function(x) { return x.match(/\s+/g) }) }
+
+findSpaces(["tori_spelling", "tori amos"]);
+// ["tori amos"]
+
+let noVowels = replace(/[aeiou]/ig);
+// function(replacement, x) { return x.replace(/[aeiou]/ig, replacement) }
+var censored = noVowels("*");
+// function(x) { return x.replace(/[aeiou]/ig, "*") }
+
+censored("Chocolate Rain");
+// 'Ch*c*l*t* R**n'
 module.exports ={
   increment,
   addTen,
   match,
   replace,
+  filter,
+  map,
+  hasSpaces,
+  findSpaces,
+  noVowels,
+  censored
 }
