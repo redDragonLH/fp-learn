f01c05558bf94a55af4eb27361b4aba2680504be
刘鹤 <liuhe@csdn.net>
2018-04-20 18:18:39 +0800

    eslint

---
+++ comm/number.js
@@ -0,0 +1,16 @@
+const ONE = 1;
+const TOW = 2;
+const THREE = 3;
+const FOUR = 4;
+const TEN = 10;
+const TWENTY = 20;
+const SIXTEEN = 16;
+module.exports = {
+  ONE,
+  TOW,
+  THREE,
+  FOUR,
+  TEN,
+  TWENTY,
+  SIXTEEN,
+};
--- unit/eight/container.js
+++ unit/eight/container.js
@@ -1,23 +1,27 @@
+
+const {TOW,THREE} = require( '../../comm/number.js' );
+
 /**
  * 容器
  * @param  {[type]} x [description]
  * @return {[type]}   [description]
  */
-let Container = function(x) {
+let Container = function( x ) {
   this.__value = x;
-}
-
-Container.of = function(x) {
-  return new Container(x); 
 };
 
-Container.of(3)
+Container.of = function( x ) {
+  return new Container( x ); 
+};
+
+console.log(THREE);
+Container.of( THREE );
 //=> Container(3)
 
 // (a -> b) -> Container a -> Container b
-Container.prototype.map = function(f){
-  return Container.of(f(this.__value));
-}
-Container.of(2).map(function(two){
-   return two + 2;
- });
+Container.prototype.map = function( f ){
+  return Container.of( f( this.__value ) );
+};
+Container.of( TOW ).map( function( two ){
+  return two + TOW;
+} );
